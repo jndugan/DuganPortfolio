bb6617800e7a4408f47b1f99b0a98e1f1e685524
Jill Dugan <jndugan@mac.com>
2017-09-04 19:31:03 -0400

    changes to background and overall html element, main menubar, smlinks

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 function substitute() {
   var myValue = document.getElementById('visitor').value;
   var thankYou = "Thank you for visiting my site, " + myValue + "! It's nice to meet you!";
-
+  var button = document.getElementById('submit')
 
   if (myValue.length == 0)  {
     alert('Name field required!');
     return;
-    }
+  }
 
   /*if (myValue.length >= 2)  {
     alert(thankYou);
